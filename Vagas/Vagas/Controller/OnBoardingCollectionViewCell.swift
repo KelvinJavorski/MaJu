146af8e02965a9f15b08668a4908219b0e47feea
Kelvin <kelvin.javorskisoares@gmail.com>
2021-01-17 19:09:21 -0300

    Changes in storyboard and onboarding cell

--- Vagas/Vagas/Controller/OnBoardingCollectionViewCell.swift
+++ Vagas/Vagas/Controller/OnBoardingCollectionViewCell.swift
@@ -8,5 +8,14 @@
 import UIKit
 
 class OnBoardingCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleTextLabel: UILabel!
+    @IBOutlet weak var explanationTextLabel: UILabel!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var buttonBackground: UIView!
     
+    @IBAction func nextButtonPressed(_ sender: Any) {
+        pageControl.currentPage += 1
+    }
 }
--- Vagas/Vagas/Controller/OnBoardingViewController.swift
+++ Vagas/Vagas/Controller/OnBoardingViewController.swift
@@ -7,23 +7,67 @@
 
 import UIKit
 
-class OnBoardingViewController: UIViewController {
-
+class OnBoardingViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        collectionView?.dataSource = self
+        collectionView?.delegate = self
+        setupCollectionView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupCollectionView(){
+        if let flowLayout = collectionView?.collectionViewLayout as? UICollectionViewFlowLayout{
+            //            flowLayout.scrollDirection = .horizontal
+        }
     }
-    */
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: "onboardingCell", for: indexPath) as! OnBoardingCollectionViewCell
+        setupCell(cell: myCell)
+        if (indexPath.row == 0){
+            myCell.imageView.image = UIImage(named: "onBoarding-1")
+            myCell.titleTextLabel.text = "Encontre estágios feitos para você"
+            myCell.explanationTextLabel.text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
+            
+        }
+        if (indexPath.row == 1){
+            myCell.backgroundColor = UIColor.green
+            
+        }
+        return myCell
+    }
+    
+    func setupCell(cell: OnBoardingCollectionViewCell){
+        cell.titleTextLabel.tintColor = UIColor(hexString: "#5BA092")
+        cell.nextButton.tintColor = .black
+        cell.nextButton.setTitle("Próximo", for: .normal)
+        cell.buttonBackground.layer.borderWidth = 1
+        cell.buttonBackground.layer.cornerRadius = 10
+    }
+    
+    @IBAction func skipTutorialPressed(_ sender: Any) {
+        
+    }
+}
 
+extension OnBoardingViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return view.frame.size
+    }
+    
+    func centerItemsInCollectionView(cellWidth: Double, numberOfItems: Double, spaceBetweenCell: Double, collectionView: UICollectionView) -> UIEdgeInsets {
+        let totalWidth = cellWidth * numberOfItems
+        let totalSpacingWidth = spaceBetweenCell * (numberOfItems - 1)
+        let leftInset = (collectionView.frame.width - CGFloat(totalWidth + totalSpacingWidth)) / 2
+        let rightInset = leftInset
+        return UIEdgeInsets(top: 0, left: leftInset, bottom: 0, right: rightInset)
+    }
+    
 }
