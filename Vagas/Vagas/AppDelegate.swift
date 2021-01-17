19d2bd1abc4ff3d25d6ab03deae609965a5dd310
Kelvin <kelvin.javorskisoares@gmail.com>
2021-01-17 10:47:15 -0300

    Added Firebase Connection

--- Vagas/Vagas/AppDelegate.swift
+++ Vagas/Vagas/AppDelegate.swift
@@ -5,16 +5,16 @@
 //  Created by Kelvin Javorski Soares on 17/01/21.
 //
 
+import Firebase
 import UIKit
 import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
--- Vagas/Vagas/ViewController.swift
+++ Vagas/Vagas/ViewController.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
+    private let database = Database.database().reference()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
 
 }
 
