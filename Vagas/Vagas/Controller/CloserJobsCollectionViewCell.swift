f886b6049a52dce1ae8043869db3cd72e220bc66
Kelvin <kelvin.javorskisoares@gmail.com>
2021-01-17 19:09:10 -0300

    Add Assets, extension for color and finished onboarding

---
+++ Vagas/Vagas/Controller/CloserJobsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CloserJobsCollectionViewCell.swift
+//  Vagas
+//
+//  Created by Kelvin Javorski Soares on 17/01/21.
+//
+
+import UIKit
+
+class CloserJobsCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Vagas/Vagas/Controller/OnBoardingCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  OnBoardingCollectionViewCell.swift
+//  Vagas
+//
+//  Created by Kelvin Javorski Soares on 17/01/21.
+//
+
+import UIKit
+
+class OnBoardingCollectionViewCell: UICollectionViewCell {
+    
+}
