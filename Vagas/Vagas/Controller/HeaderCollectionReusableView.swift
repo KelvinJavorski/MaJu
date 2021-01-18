72f5c6624233e4cbfdea4b4351090291e2fc1242
Kelvin <kelvin.javorskisoares@gmail.com>
2021-01-17 21:47:23 -0300

    Created collection for seeing every job available

---
+++ Vagas/Vagas/Controller/HeaderCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  HeaderCollectionReusableView.swift
+//  Vagas
+//
+//  Created by Kelvin Javorski Soares on 17/01/21.
+//
+
+import UIKit
+
+class HeaderCollectionReusableView: UICollectionReusableView {
+        
+}
